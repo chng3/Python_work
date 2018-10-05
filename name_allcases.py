34be8e6b8a3a16f42fceb239cb26e95abbf32643
Zhuang <chngdolphin@gmail.com>
2018-10-06 00:34:34 +0800

    add practice file

---
+++ name_allcases.py
@@ -0,0 +1,26 @@
+#2-3
+name = 'Carmelo'
+print("Hello " + name + ',' + "would you like to learn some Python today?")
+
+#2-4
+name = "carmelo anthony"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+#2-5
+name = "Chairman Mao"
+aphorism = "\"The Vast World Can Develop One's Ability To The Full.\""
+print(name + " " + "once said, " + aphorism)
+
+#2-6
+famous_person = "Chairman Mao"
+message = famous_person + " " + "once said, " + "\"The Vast World Can Develop One's Ability To The Full.\""
+print(message)
+
+#2-7
+person_name = " stephen curry "
+print(person_name.strip())
+print(person_name.lstrip())
+print(person_name.rstrip())
+print(person_name + ":" + "\n" + "\t" + person_name)
---
+++ simple_message.py
@@ -0,0 +1,9 @@
+#2-1
+message = "Do You love Me?"
+print(message)
+
+#2-2
+message = "Yes,I DO!"
+message2 = "I Got You!"
+messageall = message + message2
+print(messageall)
