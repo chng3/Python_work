a9ebd5367168ab74a3996869d114b0fb755182a9
Zhuang <chngdolphin@gmail.com>
2018-10-20 22:38:36 +0800

    practice to 6-1-3

---
+++ Chapter_6(字典)/favorite_languages.py
@@ -0,0 +1,12 @@
+#由类似对象组成的字典
+
+favorite_languages = {
+    'jen': 'Python',
+    'sarah': 'C',
+    'edward': 'ruby',
+    'phil': 'Python'
+    }
+
+print("Sarah's favorite language is " +
+      favorite_languages['sarah'].title() +
+      ".")
