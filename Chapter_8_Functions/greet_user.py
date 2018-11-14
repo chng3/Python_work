bbdf06d2649a6c8b19ac3640e4baeafb26386d97
Zhuang <chngdolphin@gmail.com>
2018-11-14 20:15:04 +0800

    practice to 8-17

--- Chapter_8_Functions/greet_user.py
+++ Chapter_8_Functions/greet_user.py
@@ -1,12 +1,29 @@
 # 8.4 传递列表
+import greet_user2
 
-def greet_users(names):
-       """向列表中的每位用户都发出简单的问候"""
-       for name in names:
-              msg = "Hello, " + name.title() + "!"
-              print(msg)
+usernames = ['hannah', 'ty', 'margot']
+greet_user2.greet_users(usernames)
 
+print("\n")
+
+from greet_user2 import greet_users
 usernames = ['hannah', 'ty', 'margot']
 greet_users(usernames)
 
+print("\n")
 
+from greet_user2 import greet_users as gu
+usernames = ['hannah', 'ty', 'margot']
+gu(usernames)
+
+print("\n")
+
+import greet_user2 as gu2
+usernames = ['hannah', 'ty', 'margot']
+gu2.greet_users(usernames)
+
+print("\n")
+
+from greet_user2 import *
+usernames = ['hannah', 'ty', 'margot']
+greet_users(usernames)
